a4dff1e05f9ec621d5d11ba3049ce7a140dcd4f7
henriquezandona-b <henrique.zandonab@gmail.com>
2026-07-01 13:07:07 -0300

    Add CC recipient to email sending function

--- api/send.js
+++ api/send.js
@@ -25,6 +25,7 @@ export default async function handler(req, res) {
     const result = await resend.emails.send({
       from: "Embalagens <onboarding@resend.dev>",
       to: "henrique.zandonab@gmail.com",
+      cc: "henrique.zandona@hyundai-brasil.com",
       subject: "Relatório de Embalagens",
       html: `<pre>${JSON.stringify(itens, null, 2)}</pre>`
     });
