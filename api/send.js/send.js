773242d21e906d22a2e37d6ef011375d99a39ee8
henriquezandona-b <henrique.zandonab@gmail.com>
2026-07-01 09:21:26 -0300

    Change API handler to ES module export
    Refactor API handler to use ES module syntax.

--- api/send.js/send.js
+++ api/send.js/send.js
@@ -1,10 +1,6 @@
-module.exports = async (req, res) => {
-  try {
-    res.status(200).json({
-      ok: true,
-      message: "API funcionando no Vercel"
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+export default function handler(req, res) {
+  res.status(200).json({
+    ok: true,
+    message: "API funcionando no Vercel"
+  });
+}
