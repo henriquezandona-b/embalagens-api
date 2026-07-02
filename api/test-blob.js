bcd9c03018b3b609aaea13ea59db42742c5f22ec
henriquezandona-b <henrique.zandonab@gmail.com>
2026-07-01 21:07:04 -0300

    Allow overwriting blobs in test-blob.js

--- api/test-blob.js
+++ api/test-blob.js
@@ -2,16 +2,22 @@ import { put } from "@vercel/blob";
 
 export default async function handler(req, res) {
   try {
-   const blob = await put("teste.txt", "Funcionou!", {
-  access: "public",
-  addRandomSuffix: false,
-  token: undefined
-});
+    const blob = await put(
+      "teste.txt",
+      "Funcionou!",
+      {
+        access: "public",
+        addRandomSuffix: false,
+        allowOverwrite: true
+      }
+    );
+
     return res.status(200).json({
       ok: true,
       url: blob.url,
       pathname: blob.pathname
     });
+
   } catch (err) {
     return res.status(500).json({
       ok: false,
