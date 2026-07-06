bdd1e90e0b76a7f5c47dccfe0d754028c75151db
henriquezandona-b <henrique.zandonab@gmail.com>
2026-07-06 08:15:02 -0300

    Update response JSON structure in send.js
    Removed 'drive' from the response JSON in send.js.

--- api/send.js
+++ api/send.js
@@ -68,11 +68,9 @@ export default async function handler(req, res) {
     });
 
     return res.status(200).json({
-      ok: true,
-      drive: excelAtualizado.drive,
-      email: result
-    });
-
+  ok: true,
+  email: result
+});
   } catch (error) {
     console.error(error);
 
