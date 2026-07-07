f2e687a47926d41e690bef66e02f2db03f19010b
henriquezandona-b <henrique.zandonab@gmail.com>
2026-07-07 10:06:20 -0300

    Update dashboard.js

--- api/dashboard.js
+++ api/dashboard.js
@@ -54,7 +54,7 @@ export default async function handler(req, res) {
         total
       }))
       .sort((a,b)=>b.total-a.total)
-      .slice(0,10);
+      .slice(0,3);
 
     return res.status(200).json({
 
